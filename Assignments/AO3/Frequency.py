f636917e2b790967d3572432381eb27d7203751d
Ponyduelist003 <42812533+Ponyduelist003@users.noreply.github.com>
2020-09-04 13:10:45 -0500

    Update and rename MyFrequency.py to Frequency.py

---
+++ Assignments/AO3/Frequency.py
@@ -0,0 +1,48 @@
+#Code skeleton copied from instructor for frequency analysis. 
+
+import sys
+import os
+import requests
+
+alphabet = [chr(x+97) for x in range(26)]
+
+class Frequency():
+    def __init__(self):
+        self.text = ""
+        self.freq = {}
+        self.sort_freq = None
+
+        for l in alphabet:
+            self.freq[l] = 0
+    
+    def count(self,text):
+        for l in text:
+            l = l.lower()
+            if l in alphabet:
+                self.freq[l] += 1
+
+        # https://realpython.com/python-lambda/
+        self.sort_freq = sorted(self.freq.items(), key=lambda x: x[1], reverse=True)
+
+    def print(self):
+        if self.sort_freq:
+            for f in self.sort_freq:
+                print(f"{f[0]}:{f[1]}")
+        else:
+            print(self.freq)
+
+    def getNth(self,n):
+        if self.sort_freq:
+            return self.sort_freq[n][0]
+
+        return None
+
+if __name__=='__main__':
+    text = " "
+    
+print("Calculating frequency...")
+F = Frequency()
+
+F.count(text)
+
+F.print()
--- Assignments/AO3/MyFrequency.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-import requests
-
-alphabet = [chr(x+97) for x in range(26)]
-
-#Copied from insturctor code for dictionary setup
