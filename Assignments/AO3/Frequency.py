a9e6796fe17beae09f33c95c25a7468bcf7c9b6a
Ponyduelist003 <42812533+Ponyduelist003@users.noreply.github.com>
2020-09-08 10:18:52 -0500

    Update Frequency.py

--- Assignments/AO3/Frequency.py
+++ Assignments/AO3/Frequency.py
@@ -4,9 +4,11 @@ import sys
 import os
 import requests
 
+#Dictionary alphabet
 alphabet = [chr(x+97) for x in range(26)]
 
 class Frequency():
+    #class initiation
     def __init__(self):
         self.text = ""
         self.freq = {}
@@ -15,6 +17,7 @@ class Frequency():
         for l in alphabet:
             self.freq[l] = 0
     
+    #counts number of each letter in text
     def count(self,text):
         for l in text:
             l = l.lower()
@@ -24,6 +27,7 @@ class Frequency():
         # https://realpython.com/python-lambda/
         self.sort_freq = sorted(self.freq.items(), key=lambda x: x[1], reverse=True)
 
+    #prints the count of letters
     def print(self):
         if self.sort_freq:
             for f in self.sort_freq:
@@ -31,15 +35,18 @@ class Frequency():
         else:
             print(self.freq)
 
+    #returns Nth most common letter
     def getNth(self,n):
         if self.sort_freq:
             return self.sort_freq[n][0]
 
         return None
 
+#put text to be analyzed into the blank quotes
 if __name__=='__main__':
     text = " "
-    
+
+#set F to be a frequency class, perform and print frequency analysis.
 print("Calculating frequency...")
 F = Frequency()
 
