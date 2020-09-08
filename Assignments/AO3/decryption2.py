884c82692ac67a088efed6ce8093e8f93608718e
Ponyduelist003 <42812533+Ponyduelist003@users.noreply.github.com>
2020-09-08 10:23:22 -0500

    Update decryption2.py

--- Assignments/AO3/decryption2.py
+++ Assignments/AO3/decryption2.py
@@ -1,7 +1,7 @@
 # list of letters
 alphabet = [chr(x+97) for x in range(26)]
 
-#list of letters to substitute
+#list of letters to substitute, initiated to - to aid guesswork
 subs = ['-'] * 26
 subs[25] = 'e'
 subs[13] = 't'
@@ -27,12 +27,15 @@ subs[3] = 'v'
 subs[19] = 'c'
 subs[17] = 'k'
 
-# here is where you would put your plain text
+# here is the plain text
 plaintext = "npz aynp fg npz euxpnzfic vyk uc qzczn fk ybb cujzc ql npz ukzwiunuzc fg npz czbgucp ykj npz nleykkl fg zdub vzk. qbzcczj uc pz mpf, uk npz kyvz fg tpyeunl ykj xffj mubb, cpzapzejc npz mzyr npefixp npz dybbzl fg npz jyerkzcc, gfe pz uc neibl puc qefnpze’c rzzaze ykj npz gukjze fg bfcn tpubjezk. ykj u mubb cneurz jfmk iafk npzz munp xezyn dzkxzyktz ykj gieufic ykxze npfcz mpf ynnzvan nf afucfk ykj jzcnefl vl qefnpzec. ykj lfi mubb rkfm u yv npz bfej mpzk u byl vl dzkxzyktz iafk lfi.".lower()
 
+#loop through the letters of plaintext
 for p in plaintext:
+  #if a letter, take ASCII code - 97 and replace with that numbered subs entry
   if p in alphabet:
     i = ord(p)-97
     print(subs[i], end = '')
+  #if not a letter, print normally
   else:
     print(p)
